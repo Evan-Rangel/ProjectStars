f24da82a756c5a1758ed56488bfebbbc182ad245
Evan-Rangel <evan.rangel@uniat.edu.mx>
2024-03-06 08:58:03 -0600

    EliminarProyecto

--- Assets/Main/Player/PowerUps/Scripts/W_PowerEffect.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class W_PowerEffect : ScriptableObject
-{
-    public abstract void Apply(GameObject target);
-}
