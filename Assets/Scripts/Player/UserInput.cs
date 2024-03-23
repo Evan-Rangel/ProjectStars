1a183f71783c9ce5151939d1b60216140a7bfacb
Evan-Rangel <evan.rangel@uniat.edu.mx>
2024-03-23 14:16:26 -0600

    settings

---
+++ Assets/Scripts/Player/UserInput.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class UserInput : MonoBehaviour
+{
+    public static UserInput instance;
+
+    public Vector2 MoveInput { get; private set; }
+    public Vector2 LookInput { get; private set; }
+    public bool FireInput { get; private set; }
+    public bool JumpInput { get; private set; }
+
+
+    private PlayerInput _playerInput;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        _playerInput = GetComponent<PlayerInput>();
+    }
+    private void Update()
+    {
+        UpdateInputs();
+    }
+
+    private void UpdateInputs()
+    {
+        MoveInput = _playerInput.actions["Move"].ReadValue<Vector2>();
+        LookInput = _playerInput.actions["Look"].ReadValue<Vector2>();
+        FireInput = _playerInput.actions["Fire"].WasPressedThisFrame();
+        JumpInput = _playerInput.actions["Jump"].IsPressed();
+    }
+}
