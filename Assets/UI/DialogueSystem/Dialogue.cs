45705e5b7fdf5acaf027c2f5f62af5208538c0b5
Evan-Rangel <evan.rangel@uniat.edu.mx>
2024-04-22 22:24:42 -0600

    Inicio de Dialogue System

---
+++ Assets/UI/DialogueSystem/Dialogue.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    [SerializeField] Image characterImage;
+    [SerializeField] Image backgroundImage;
+    [SerializeField] TMP_Text dialogueText;
+    [SerializeField] string textToWrite;
+    int index = 0;
+    char[] text;
+
+    private void Start()
+    {
+        StarToWrite();
+    }
+    public void StarToWrite()
+    {
+        text= textToWrite.ToCharArray();
+        dialogueText.text = "";
+        index = 0;
+        StartCoroutine(NextLetter());
+    }
+    IEnumerator NextLetter()
+    {
+        yield return new WaitForSeconds(0.05f);
+        dialogueText.text += text[index]; 
+        index++;
+        if (index < text.Length)
+        {
+            StartCoroutine(NextLetter());
+        }
+    }
+}
